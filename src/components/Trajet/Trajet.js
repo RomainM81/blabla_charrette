9e34470e3731fe2280632eb8c9b24cf8cecdcb9a
mystere974 <m.loghann@gmail.com>
2021-08-06 00:03:17 +0200

    add make up in post_trajet

--- src/components/Trajet/Trajet.js
+++ src/components/Trajet/Trajet.js
@@ -1,49 +1,55 @@
+import { useState } from 'react';
+
+import parchemin from '../../assets/paper.jpg';
 import './Trajet.css';
 
 
 
 function Trajet () {
+    const [showDetail, setShowDetail]= useState()
     return (
-        <form className="formContainer">
+    <form className="formContainer">
+        <img src={parchemin} alt="parchemin" />
+        <div className="formContent">
+                <h2>Où allez-vous glairer?</h2>
             <div className="formItems">
-            
             <select className="input" type="text" placeholder="lieu de départ" required>
-                <option value=""></option>
-                <option value="">Kaamelot</option>
-                <option value="">Fort Boyard</option>
-                <option value="">Carmelid</option>
-                <option value="">Royaume de Logre</option>
-                <option value="">Versailles</option>
-                <option value="">Lancaster</option>
-                <option value="">Westminster</option>
-                <option value="">Montmirail</option>
-                <option value="">Cambridge</option>
-                <option value="">Connemara</option>
+                <option disabled selected>tu par dou?</option>
+                <option>Kaamelot</option>
+                <option>Fort Boyard</option>
+                <option>Carmelid</option>
+                <option>Royaume de Logre</option>
+                <option>Versailles</option>
+                <option>Lancaster</option>
+                <option>Westminster</option>
+                <option>Montmirail</option>
+                <option>Cambridge</option>
+                <option>Connemara</option>
             </select>
             </div>
             <div className="formItems">
             <select className="input" type="text" placeholder="destination" required>
-                <option value=""></option>
-                <option value="">Kaamelot</option>
-                <option value="">Fort Boyard</option>
-                <option value="">Carmelid</option>
-                <option value="">Royaume de Logre</option>
-                <option value="">Versailles</option>
-                <option value="">Lancaster</option>
-                <option value="">Westminster</option>
-                <option value="">Montmirail</option>
-                <option value="">Cambridge</option>
-                <option value="">Connemara</option>
+                <option disabled selected>tu ve aller ou?</option>
+                <option>Kaamelot</option>
+                <option>Fort Boyard</option>
+                <option>Carmelid</option>
+                <option>Royaume de Logre</option>
+                <option>Versailles</option>
+                <option>Lancaster</option>
+                <option>Westminster</option>
+                <option>Montmirail</option>
+                <option>Cambridge</option>
+                <option>Connemara</option>
             </select>           
             </div>
             <div className="formItems">
             <select className="input" type="text" placeholder="type de transport" required>
-                <option value=""></option>
-                <option value="">A pied</option>
-                <option value="">En chariotte</option>
-                <option value="">En calèche</option>
-                <option value="">A cheval</option>
-                <option value="">En carrosse</option>
+                <option disabled selected>comment ty va?</option>
+                <option>A pied</option>
+                <option>En chariotte</option>
+                <option>En calèche</option>
+                <option>A cheval</option>
+                <option>En carrosse</option>
             </select>       
             </div>
             <div className="formItems">
@@ -51,17 +57,20 @@ function Trajet () {
             </div>
             <div className="formItems">
             <select className="input" type="text" placeholder="nombre de passager" required>
-                <option value=""></option>
-                <option value="">1 paysan</option>
-                <option value="">2 pequeux</option>
-                <option value="">3 peqneaux</option>
-                <option value="">4 bouseux</option>
-                <option value="">5 pignoufs</option>
-                <option value="">6 hérétiques</option>
-                <option value="">+ de 6 hérétiques</option>
-                </select>       
+                <option disabled selected>tu a dla place?</option>
+                <option>1 paysan</option>
+                <option>2 pequeux</option>
+                <option>3 peqneaux</option>
+                <option>4 bouseux</option>
+                <option>5 pignoufs</option>
+                <option>6 hérétiques</option>
+                <option>+ de 6 hérétiques</option>
+                </select><br/><br/>
+                <button onClick={(event) => setShowDetail(event.target.value)}>Confirmer</button>
+                {console.log(setShowDetail)}
             </div>
-        </form>
+        </div>
+    </form>
     )
 }
 
