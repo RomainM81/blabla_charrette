1b45cbe889c53533bdc550e56650159d5134753f
Shawn <shawn17@live.fr>
2021-08-06 13:05:50 +0200

    j'en ai marre v7

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import{ useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
 import './Contact.css'
 
 const Contact = () => {
+
+  const [fox, setFox] = useState()
+
+    useEffect(() => {
+        fetch("https://randomfox.ca/floof/")
+        .then(response => response.json())
+        .then(data => setFox(data))
+    }, [])
+
+    const [abdou, setAbdou] = useState(false)
+
     return (
 
         <div className="contact">
             <h2>Pour plus d'information,</h2>
             <h2>contactez Abdou,</h2>
-            <h2>il est sur le retour... &#127939; &#128660;</h2>
+            <h2>il est sur le retour... &#127939; <span onClick={() => setAbdou(true)}>&#128660;</span></h2>
 
             <img src="https://media.giphy.com/media/sUt8uDIusftiE/giphy.gif" alt="" />
             <img src="https://media.giphy.com/media/ntxtuZ2NDW2uQ/giphy.gif" alt="" />
             <img src="https://media.giphy.com/media/RYjnzPS8u0jAs/giphy.gif" alt="" />
 
+            <h3>La Bise les Gueux !</h3>
+
+             {abdou ? <img src={fox.image} alt="abdou fetch" /> : null }
         </div>
 
 
