e7ef7e05d986c56da676cd8e3c6c3221da09a122
Shawn <shawn17@live.fr>
2021-08-06 11:51:55 +0200

    j'en ai marre v4.1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
         <div className="contact">
             <h2>Pour plus d'information,</h2>
             <h2>contactez Abdou,</h2>
-            <h2>il est sur le retour... &#128660;</h2>
+            <h2>il est sur le retour... &#127939; &#128660;</h2>
 
             <img src="https://media.giphy.com/media/sUt8uDIusftiE/giphy.gif" alt="" />
             <img src="https://media.giphy.com/media/ntxtuZ2NDW2uQ/giphy.gif" alt="" />
