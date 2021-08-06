9adf04f0a4e55f29f03505b556264ff8ebe55316
Ludivine BENVENUTI <ludivine.benvenuti@hotmail.fr>
2021-08-06 11:19:14 +0200

    Ajout gif sur page de contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,11 +8,14 @@ const Contact = () => {
     return (
 
         <div className="contact">
-            <h2>Pour plus d'information,
-            contactez Abdou,
-            il est sur le retour...</h2>
+            <h2>Pour plus d'information,</h2>
+            <h2>contactez Abdou,</h2>
+            <h2>il est sur le retour... &#128660;</h2>
 
-            <img src="https://media.giphy.com/media/KbAkMMoVyry2tIcaE0/giphy.gif" alt="" />
+            <img src="https://media.giphy.com/media/sUt8uDIusftiE/giphy.gif" alt="" />
+            <img src="https://media.giphy.com/media/ntxtuZ2NDW2uQ/giphy.gif" alt="" />
+            <img src="https://media.giphy.com/media/RYjnzPS8u0jAs/giphy.gif" alt="" />
+
         </div>
 
 
